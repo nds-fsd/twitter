0fc987db6fcfbe6c0f3723711b188e8330ae4a1c
mbedia94 <mbedia94@gmail.com>
2023-11-29 02:07:35 +0100

    feature(backend-schema-user):creacion schema user

--- backend/src/schemas/user.js
+++ backend/src/schemas/user.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
-  userame: { type: String, required: true },
+  username: { type: String, required: true },
   birthday: { type: String, required: true },
   mail: { type: String, required: true },
   password: { type: String, required: true },
