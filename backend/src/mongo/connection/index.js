d4286a81ebf8233c355bf31df094db8bc5c3376b
mbedia94 <mbedia94@gmail.com>
2023-11-29 02:00:52 +0100

    feature(backend-schema-user):creacion schema user

--- backend/src/mongo/connection/index.js
+++ backend/src/mongo/connection/index.js
@@ -1,31 +1,29 @@
-const mongoose =  require('mongoose');
+const mongoose = require("mongoose");
 
 let dbUrl = process.env.MONGO_URL;
 
 let mongodb;
 
-exports.connectDB = async() => {
-    mongoose.set("strictQuery", false);
+exports.connectDB = async () => {
+  mongoose.set("strictQuery", false);
 
-    try {
-        await   mongoose.connect(dbUrl);
+  try {
+    await mongoose.connect(dbUrl);
 
-        const mongo = mongoose.connection;
-        mongo.on("error", (error) => console.error(error));
-    }catch (e){
-        console.log(e);
-    }
-}
-
-
-exports.disconnectDB = async () => {
-    try {
-        await mongoose.connection.close();
-        if (mongodb) {
-            await mongodb.stop();
-        }
-    } catch (err) {
-        console.log(err);
-    }
+    const mongo = mongoose.connection;
+    mongo.on("error", (error) => console.error(error));
+  } catch (e) {
+    console.log(e);
+  }
 };
 
+exports.disconnectDB = async () => {
+  try {
+    await mongoose.connection.close();
+    if (mongodb) {
+      await mongodb.stop();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ backend/src/schemas/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  userame: { type: String, required: true },
+  birthday: { type: String, required: true },
+  mail: { type: String, required: true },
+  password: { type: String, required: true },
+  description: { type: String, required: true },
+  dateOfRegister: { type: String, required: false },
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
