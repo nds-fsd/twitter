b37d48ab15d664c5b969da49b24fb874eacba89b
Adrian Castan <141574038+AdrianCastanRuiz@users.noreply.github.com>
2023-12-05 19:23:59 +0100

    NewCommit

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -17,10 +17,13 @@ const RegisterForm = ()=> {
 
        }
 
-
+// Hay medidas provisionales que hay que eliminar, como en el primer div(los pixels)
        return(
-        <div >
+        <div style={{marginLeft: "500px", marginTop: "40px"}}> 
             <h2>Create your account</h2>
+
+            <br />
+            <br />
            
            
            
@@ -30,18 +33,20 @@ const RegisterForm = ()=> {
                 
                     <div>
                     <input maxLength={30} type="text" name="" placeholder="Name"  {...register("name", { required: true })}  />
-                    {errors.name?.type === 'required' && <p className={styles.error}>Name is required</p>}
+                    {errors.name?.type === 'required' && <p className={styles.error}>Name is required.</p>}
                     </div>
                    
                    
                    <div> 
                    <input maxLength={30} type="text" name="" placeholder="Surname" {...register("surname", { required: true, })} />
-                   {errors.surname?.type === 'required' && <p className={styles.error}>Surname is required</p>} 
+                   {errors.surname?.type === 'required' && <p className={styles.error}>Surname is required.</p>} 
                    </div>
 
                   <div>
-                    <input maxLength={80} type="text" name="" placeholder="Email" {...register("mail", { required: true, })} />
-                    {errors.mail?.type === 'required' && <p className={styles.error}>Mail is required</p>}
+                    <input maxLength={80} type="text" name="" placeholder="Email" {...register("mail", { required: true,
+                    pattern: '/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g' })} />
+                    {errors.mail?.type === 'required' && <p className={styles.error}>Mail is required.</p>}
+                    {errors.mail?.type === 'pattern' && <p className={styles.error}>Please enter a valid email.</p>}
                   </div>
                   
                    <div className={styles.date}>
@@ -49,13 +54,13 @@ const RegisterForm = ()=> {
                    <input type="date" {...register("date", { required: true, })} />
                    {errors.date?.type === 'required' && <p className={styles.error}>Date is required</p>}
                   </div>
-
-
+                   <br />
+                   <br />
 
                   <h3  style={{color: "white"}}>Username and password</h3>
 
                   <div>
-                    <input maxLength={20} type="text" placeholder='Username' {...register("username", { required: true, })} />
+                    <input maxLength={20} type="text" placeholder='Username' {...register("username", { required: true })}/>
                     {errors.username?.type === 'required' && <p className={styles.error}>Username is required</p>}
                   </div>
 
@@ -63,6 +68,7 @@ const RegisterForm = ()=> {
                       <input maxLength={30} type="password" placeholder='Password' {...register("password", { required: true,
                       minLength: 8 })} />
                       {errors.password?.type === 'required' && <p className={styles.error}>Password is required</p>}
+                      {errors.password?.type === 'minLength' && <p className={styles.error}>Password must be 8 to 30 character long.</p>}
                       </div>
                      
                       <div>
@@ -71,7 +77,12 @@ const RegisterForm = ()=> {
                       {errors.passwordConfirm?.type === 'required' && <p className={styles.error}>Please, confirm your password</p>}
                   </div>
 
-                  <input type="submit" value={"Sign up"}></input>
+                  <div>
+                  <input className={styles.submit} type="submit" value={"Sign up"}></input>
+                  </div>
+              
+
+                  
                 
                 </form>
         
