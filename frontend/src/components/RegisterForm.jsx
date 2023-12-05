dc780f85fd9b2b1d01d2bcaa6e304cfd3b939010
Adrian Castan <141574038+AdrianCastanRuiz@users.noreply.github.com>
2023-12-05 18:23:16 +0100

    newCommit

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -8,32 +8,43 @@ import React, { useState } from "react";
 const RegisterForm = ()=> {
 
  
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+       const onSubmit = (data)=> {
+
+        console.log(data)
 
 
+       }
 
-    return(
+
+       return(
         <div >
             <h2>Create your account</h2>
            
            <h3 style={{color: "white"}}>Personal details</h3>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                 
                     <div>
-                    <input type="text" name="" placeholder="Name"/>
+                    <input type="text" name="" placeholder="Name"  {...register("name", { required: true, })}  />
+                    {errors.name?.type === 'required' && <p className={styles.error}>Name is required</p>}
                     </div>
                    
+                   
                    <div> 
-                   <input type="text" name="" placeholder="Surname"/> 
+                   <input type="text" name="" placeholder="Surname" {...register("surname", { required: true, })} />
+                   {errors.surname?.type === 'required' && <p className={styles.error}>Surname is required</p>} 
                    </div>
 
                   <div>
-                    <input type="text" name="" placeholder="Email" />
+                    <input type="text" name="" placeholder="Email" {...register("mail", { required: true, })} />
+                    {errors.mail?.type === 'required' && <p className={styles.error}>Mail is required</p>}
                   </div>
                   
                    <div className={styles.date}>
                    <label htmlFor="">Date of birth</label>
-                   <input type="date" />
+                   <input type="date" {...register("date", { required: true, })} />
+                   {errors.date?.type === 'required' && <p className={styles.error}>Date is required</p>}
                   </div>
 
 
@@ -41,21 +52,27 @@ const RegisterForm = ()=> {
                   <h3  style={{color: "white"}}>Username and password</h3>
 
                   <div>
-                    <input type="text" placeholder='Username' />
+                    <input type="text" placeholder='Username' {...register("username", { required: true, })} />
+                    {errors.username?.type === 'required' && <p className={styles.error}>Username is required</p>}
                   </div>
 
-                  <div>
-                    <input type="password" placeholder='Password' />
-                    <input type="password" placeholder='Confirm your password'/>
+                      <div>
+                      <input type="password" placeholder='Password' {...register("password", { required: true,
+                      minLength: 8 })} />
+                      {errors.password?.type === 'required' && <p className={styles.error}>Password is required</p>}
+                      {errors.password?.type === 'minLength' && <p className={styles.error}>Passwrod must be 8 caracters long</p>}
+                      </div>
+                     
+                      <div>
+                    <input type="password" placeholder='Confirm your password' {...register("passwordConfirm",
+                    { required: true, })} />
+                      {errors.passwordConfirm?.type === 'required' && <p className={styles.error}>Please, confirm your password</p>}
                   </div>
 
+                  <input type="submit" value={"Sign up"}></input>
                 
-                  
-  
-                
-                
-
-            </form>
+                </form>
+        
         </div>
 
     )
