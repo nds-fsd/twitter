8259aa3b361a95c8c1de755c152a062f6ccaf0d5
bethrb2003 <beth.remeseiro.2003@gmail.com>
2023-12-07 18:35:26 +0100

    listado de tweets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,12 @@
-import { useState } from "react"
-import { useEffect } from "react";
-import { meowApi } from "./meowsApi/apiiWrapper"
-import { postMeow, updateMeow, deleteMeow } from "./meowsApi/meowsRequests";
-
+import Meows from "./components/Meows"
 
 function App() {
-
-  const [meows, setMeows] = useState('');
-  const [error, setError] = useState(false)
-  const [errorMessage, seterrorMessage] = useState('')
-
-
-
-useEffect(()=> {
-   const getAllMeows = async ()=> {
-
-      try{
-          const res = await meowApi.get('/');
-          const data = res.data
-  
-          
-          setMeows(data)
-          
-         
-     } catch(error){
-       
-        console.log(error)
-        setError(true)
-        seterrorMessage(error.message)
-     }
-
-     
-   
-          }
-   getAllMeows()
-
-}, [])
-
-
-   if (error) return(
-      <div style={{fontSize:'40px'}}>Ops, something went wrong!
-         <p style={{fontSize:'20px', color: 'red', fontWeight: 'bold'}}>{errorMessage}</p>
-      </div>
-   )
    
          return (
-         
-            meows&&meows.map( meow =>{
-
-               return <div key={meow._id}>
-                  {meow.text}<br/>
-                  <span>{meow.reposts}</span><span>{meow.likes}</span><span>{meow.views}</span>
-                  {meow.date}
-               </div>
-
-               
-            }))
+            <div>
+               <Meows />
+            </div>
+            )
  
     
 }
