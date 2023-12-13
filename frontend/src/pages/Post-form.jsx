8683de6fcdbef86e7d74c698352dec9a5c3a627f
cecilia-lorenzo <cecilia.lorenzo.galarza@gmail.com>
2023-12-13 22:40:27 +0100

    fix(styles):Postform css fix

--- frontend/src/pages/Post-form.jsx
+++ frontend/src/pages/Post-form.jsx
@@ -13,11 +13,11 @@ function PostForm () {
         <div className={styles.container}>
             <div className={styles.containerPost}>
                 <img className={styles.accountImage} src={image} alt="Profile Photo" />
-                <textarea  className={styles.postInput} name="text" id="text" cols="1" rows="5" placeholder="What is on your mind?"></textarea>
-            </div>onChange={handleKeyDown}
+                <textarea onChange={handleKeyDown} className={styles.postInput} name="text" id="text" cols="1" rows="5" placeholder="What is on your mind?"></textarea>
+            </div>
             <div className={styles.containerButtons}>
                 <div>
-                    <p>text</p>
+                    <p> </p>
                 </div>                
                 <button className={styles.postButton}>Post</button>
             </div>
