f4b0baf8468286ecdcc19ef4abcb51d24cb970b9
Adrian Castan <141574038+AdrianCastanRuiz@users.noreply.github.com>
2023-12-28 18:01:42 +0100

    new

--- backend/src/schemas/user.js
+++ backend/src/schemas/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
@@ -11,6 +12,31 @@ const userSchema = new Schema({
   dateOfRegister: { type: String, required: true },
 });
 
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, function(err, salt){
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) return next(err);
+
+      user.password = hash;
+      next();
+    })
+  })
+
+  
+} );
+
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
 const User = model("user", userSchema);
 
 module.exports = User;
+
+
