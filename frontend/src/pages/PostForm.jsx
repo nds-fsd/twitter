7879f80215de9dd288daae8ea77637c0c9b75745
cecilia-lorenzo <cecilia.lorenzo.galarza@gmail.com>
2024-01-16 15:09:20 +0100

    feature(PostForm): improvements in element

--- frontend/src/pages/PostForm.jsx
+++ frontend/src/pages/PostForm.jsx
@@ -1,11 +1,49 @@
 import styles from "./PostForm.module.css";
 import image from "../assets/Elon-Musk.jpg";
+import { useState } from "react";
+import { event } from "jquery";
 
 function PostForm() {
   function handleKeyDown(e) {
     e.target.style.height = "inherit";
     e.target.style.height = `${e.target.scrollHeight}px`;
   }
+
+  const [text, setText] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [errors, setErrors] = useState({ text: "" });
+  const [responseError, setResponseError] = useState(false);
+
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log(text);
+    const data = { text, id };
+    let error = { ...errors };
+
+    if (!text) {
+      setErrors({ text: "Text required" });
+    }
+
+    fetch("", {
+      method: "POST",
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status !== 200) {
+          setResponseError(true);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setResponseError(true);
+      });
+  };
 
   return (
     <div className={styles.container}>
@@ -25,7 +63,9 @@ function PostForm() {
         <div>
           <p> </p>
         </div>
-        <button className={styles.postButton}>Post</button>
+        <button className={styles.postButton} onSubmit={handleSubmit}>
+          Post
+        </button>
       </div>
     </div>
   );
