04d6c0da69f449d702f9ab4665ae7a441e50f1a6
cecilia-lorenzo <cecilia.lorenzo.galarza@gmail.com>
2024-01-17 23:46:44 +0100

    fix(Navbar+MeowsFiler): CSS improvements and delete comment in NavBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,8 @@
 import styles from "./Navbar.module.css";
 import home from "../assets/hogar.png";
-import busqueda from "../assets/busqueda.png";
 import campana from "../assets/campana.png";
-import marcador from "../assets/marcador.png";
-import menu from "../assets/menu.png";
 import sobre from "../assets/sobre.png";
-import square from "../assets/square.png";
 import usuario from "../assets/usuario.png";
-import user from "../assets/user.png";
 import cat from "../assets/Cat.png";
 import { removeSession } from "../local-storage";
 import { useContext } from "react";
@@ -25,16 +20,12 @@ function Navbar() {
             <img src={home} alt="." />
             <p>Home</p>
           </div>
-          {/* <div className={styles.options}><img src={busqueda} alt="."/><p>Explore</p></div>
-                <div className={styles.options}><img src={campana} alt="."/><p>Notifications</p></div>
-                <div className={styles.options}><img src={sobre} alt="."/><p>Messages</p></div>
-                <div className={styles.options}><img src={square} alt="."/><p>Lists</p></div>
-                <div className={styles.options}><img src={marcador} alt="."/><p>Booksmarks</p></div> */}
+          {/*<div className={styles.options}><img src={campana} alt="."/><p>Notifications</p></div>
+              <div className={styles.options}><img src={sobre} alt="."/><p>Messages</p></div>*/}
           <div className={styles.options}>
             <img src={usuario} alt="." />
             <p>Profile</p>
           </div>
-          {/* <div className={styles.options}><img src={menu} alt="."/><p>More</p></div> */}
           {/* <button>Post</button> */}
         </div>
       </nav>
