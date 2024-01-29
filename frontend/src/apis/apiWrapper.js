33946befcfee6a7be2c35eedcac6bc2dd87095b2
Jose Manuel Cano <jmcano94@gmail.com>
2024-01-29 19:46:42 +0100

    change wrapper and add env variable for BACKEND_URL

--- frontend/src/apis/apiWrapper.js
+++ frontend/src/apis/apiWrapper.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL
+
 export const userApi = axios.create({
-  baseURL: "http://localhost:3001/user",
+  baseURL: `${BASE_URL}/user`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const userRegisterApi = axios.create({
-  baseURL: "http://localhost:3001/user/register",
+  baseURL: `${BASE_URL}/user/register`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const meowApi = axios.create({
-  baseURL: "http://localhost:3001/meow",
+  baseURL: `${BASE_URL}/meow`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const followApi = axios.create({
-  baseURL: "http://localhost:3001/follow",
+  baseURL: `${BASE_URL}/follow`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const likeApi = axios.create({
-  baseURL: "http://localhost:3001/like",
+  baseURL: `${BASE_URL}/like`,
   headers: { "Content-Type": "application/json" },
 });
