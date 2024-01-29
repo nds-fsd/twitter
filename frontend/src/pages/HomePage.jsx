23bb3ba4878b0217a55701fa67ca92d002342e99
bethrb2003 <beth.remeseiro.2003@gmail.com>
2024-01-29 21:40:18 +0100

    cambios en homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import MeowsFilter from "../components/MeowsFilter";
 import PostForm from "./PostMeow";
 import Meows from "./Meows";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 70) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeColor);
+
   return (
-    <div>
-      <MeowsFilter />
+    <div className={styles.mainContainer}>
+      {/* <MeowsFilter /> */}
+      <div className={color ? styles.header : styles.container}>
+        <p>Here are your Meows!</p>
+      </div>
       <PostForm />
       <Meows />
     </div>
