44ae7e6731dc2478684537a91ea59720a824f5bc
bethrb2003 <beth.remeseiro.2003@gmail.com>
2024-02-07 19:48:02 +0100

    new

--- frontend/src/pages/PostMeow.jsx
+++ frontend/src/pages/PostMeow.jsx
@@ -11,21 +11,6 @@ function PostForm() {
   const [error, setError] = useState(false);
   const token = getUserToken();
   const reload = useContext(context);
-  const [color, setColor] = useState(false);
-  const changeColor = () => {
-    if (window.scrollY >= 70) {
-      setColor(true);
-    } else {
-      setColor(false);
-    }
-  };
-
-  window.addEventListener("scroll", changeColor);
-
-  function handleKeyDown(e) {
-    e.target.style.height = "inherit";
-    e.target.style.height = `${e.target.scrollHeight}px`;
-  }
 
   const postNewMeow = async () => {
     try {
@@ -56,9 +41,6 @@ function PostForm() {
   return (
     <div className={styles.container}>
       <div className={styles.containerPost}>
-        <div className={color ? styles.header : styles.fixed}>
-          <p>Here are your Meows!</p>
-        </div>
         <img className={styles.accountImage} src={image} alt="Profile Photo" />
         <textarea
           value={newMeow}
