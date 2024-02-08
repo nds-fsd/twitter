a562e11c4cd5b414592da288578b04c2b8f8ae72
AdrianCastanRuiz <141574038+AdrianCastanRuiz@users.noreply.github.com>
2024-02-08 10:15:00 +0100

    new

--- frontend/src/components/Meows/DeleteEditMeow.jsx
+++ frontend/src/components/Meows/DeleteEditMeow.jsx
@@ -1,15 +1,23 @@
 import styles from "./DeleteEditMeow.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import options from "../../assets/options.jpg";
 import del from "../../assets/delete.png";
 import edit from "../../assets/edit.png";
 
 const DeleteEditMeow = ({ meow }) => {
   const [popOut, setPopOut] = useState(false);
+  const divRef = useRef();
+  document.addEventListener("click", (e) => {
+    if (divRef.current && divRef.current.contains(e.target)) {
+      return;
+    } else {
+      setPopOut(false);
+    }
+  });
 
   return (
     <>
-      <div className={styles.container}>
+      <div ref={divRef} className={styles.container}>
         <img
           src={options}
           onClick={() => setPopOut(!popOut)}
