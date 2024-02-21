23ab1530f48015ab56fd4e81b3633569fe867107
cecilia-lorenzo <cecilia.lorenzo.galarza@gmail.com>
2024-02-21 16:50:50 +0100

    chore(mailing): key

--- backend/src/service/email-service/index.js
+++ backend/src/service/email-service/index.js
@@ -1,7 +1,7 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: MAILGUN_KEY});
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_KEY});
 const fs = require("fs");
 const Handlebars = require('handlebars');
 const path = require('path');
