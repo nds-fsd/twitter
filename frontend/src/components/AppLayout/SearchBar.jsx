d3c514cf5832245a667831299a5fa7a9677801d8
Adrian-Castan <141574038+AdrianCastanRuiz@users.noreply.github.com>
2024-02-28 12:01:00 +0100

    new! ;:DDDDD :DDD

--- frontend/src/components/AppLayout/SearchBar.jsx
+++ frontend/src/components/AppLayout/SearchBar.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { userApi } from "../../functions/apiWrapper";
 import styles from "./SearchBar.module.css";
+import { useNavigate } from "react-router-dom";
 
 function SearchBar() {
   const [color, setColor] = useState(false);
   const [substring, setSubstring] = useState("");
   const [result, setResult] = useState([]);
+  const containerRef = useRef(null);
+  const navigate = useNavigate();
 
   const changeColor = () => {
     if (window.scrollY >= 70) {
@@ -17,6 +20,7 @@ function SearchBar() {
 
   const searchUsernames = async (e) => {
     const value = e.target.value;
+    if(!value) return setResult([])
     setSubstring(value); // Actualizamos el estado substring con el valor del campo de entrada
 
     try {
@@ -24,17 +28,39 @@ function SearchBar() {
       setResult(response.data);
       console.log(response.data);
     } catch (error) {
-      console.error("Error al realizar la búsqueda:", error);
+      console.error("Error fetching data:", error);
     }
   };
 
-  window.addEventListener("scroll", changeColor);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.tagName === "SPAN") navigate(`user/${event.target.id}`)
+      if (containerRef.current && !containerRef.current.contains(event.target)) {
+       setResult([])
+       setSubstring('')
+        console.log("Clic fuera del contenedor");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [containerRef]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
 
   return (
     <>
-      <div className={color ? styles.searchScroll : styles.search}>
+      <div ref={containerRef} id="container" className={color ? styles.searchScroll : styles.search}>
         <input
           className={styles.buscador}
+          value={substring}
           onChange={(e) => searchUsernames(e)} // Llamamos a la función searchUsernames en el evento onChange
           placeholder="Search"
           type="text"
@@ -44,8 +70,8 @@ function SearchBar() {
         <div className={styles.results}>
           {result.map((user) => {
             return (
-              <p className={styles.listOfUsers}>
-                {user.name} {user.surname} (@{user.username})
+              <p className={styles.listOfUsers} key={user.username}>
+                <span id={user.username} >{user.name} {user.surname}</span> (@{user.username})
               </p>
             );
           })}
