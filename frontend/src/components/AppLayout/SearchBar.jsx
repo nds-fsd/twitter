4db07fbbb67175f4c37487a644e86bbc7a801858
Adrian-Castan <141574038+AdrianCastanRuiz@users.noreply.github.com>
2024-02-28 21:23:52 +0100

    fix(search)

--- frontend/src/components/AppLayout/SearchBar.jsx
+++ frontend/src/components/AppLayout/SearchBar.jsx
@@ -33,21 +33,7 @@ function SearchBar() {
     }
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (event.target.tagName === "SPAN") navigate(`user/${event.target.id}`)
-      if (containerRef.current && !containerRef.current.contains(event.target)) {
-       setResult([])
-       setSubstring('')
-        console.log("Clic fuera del contenedor");
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [containerRef]);
+  
 
   useEffect(() => {
     window.addEventListener("scroll", changeColor);
@@ -58,7 +44,9 @@ function SearchBar() {
 
   return (
     <>
-      <div ref={containerRef} id="container" className={color ? styles.searchScroll : styles.search}>
+    <div ref={containerRef} onMouseLeave={()=>{ setResult([])
+                                           setSubstring('') } }  >
+    <div id="container" className={color ? styles.searchScroll : styles.search}>
         <input
           className={styles.buscador}
           value={substring}
@@ -72,12 +60,17 @@ function SearchBar() {
           {result.map((user) => {
             return (
               <p className={styles.listOfUsers} key={user.username}>
-                <span id={user.username} >{user.name} {user.surname}</span> (@{user.username})
-              </p>
+                <span onClick={()=> {navigate(`/user/${user.username}`)
+                         setResult([])
+                            setSubstring('')}} id={user.username} > 
+                        {user.name} {user.surname}</span> (@{user.username})
+                     </p>
             );
           })}
         </div>
       )}
+       </div>
+    
     </>
   );
 }
